5f5f70225f43cf4b32a58e44ef424fb38fa4d61e
osy9757 <osy9757@gmail.com>
2023-11-06 14:51:32 +0900

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          test
         </p>
         <a
           className="App-link"
@@ -16,7 +17,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
+          test
         </a>
       </header>
     </div>
