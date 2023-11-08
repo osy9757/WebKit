8c442ceaae2a4598f220e824a5b963c41325fad8
osy <osy9757@gmail.com>
2023-11-09 01:16:00 +0900

    FEAT : basic header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Header from "./components/Header";
+
 import MainPage from './pages/MainPage';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/main" />} />
-          <Route path="/main" element={<MainPage />} />
-          {/* 추가적인 라우트들을 여기에 작성할 수 있습니다. */}
-        </Routes>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/main" />} />
+            <Route path="/main" element={<MainPage />} />
+            {/* 추가적인 라우트들을 여기에 작성할 수 있습니다. */}
+          </Routes>
       </div>
     </BrowserRouter>
   );
