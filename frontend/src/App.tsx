5a6c803ea939f9ce7f493140bb232132f5716113
osy9757 <osy9757@gmail.com>
2023-11-13 10:26:08 +0900

    ERROR : App.tsx merge conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-
-=======
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
->>>>>>> 8c442ceaae2a4598f220e824a5b963c41325fad8
-
 import Header from "./components/Header";
 
 import MainPage from './pages/MainPage';
 
 const App: React.FC = () => {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <Routes>
-          {/* / 으로 접근하든 /main 으로 접근하든 홈으로 이동할 수 있게 설정 */}
-          <Route path="/" element={<Navigate to="/main" replace />} />
-          {/* 홈/메인 */}
-          <Route path="/main" element={<Home />} />
-        </Routes>
-      </header>
-    </div>
-=======
     <BrowserRouter>
       <div className="App">
         <Header />
@@ -35,8 +17,8 @@ const App: React.FC = () => {
           </Routes>
       </div>
     </BrowserRouter>
->>>>>>> 8c442ceaae2a4598f220e824a5b963c41325fad8
   );
 }
 
 export default App;
+//
